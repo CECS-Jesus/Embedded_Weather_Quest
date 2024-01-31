6ec4746379dc74a749f184e41880216c858ece8d
CECS-Jesus <119976553+CECS-Jesus@users.noreply.github.com>
2024-01-30 20:40:51 -0800

    uploaded header files

---
+++ inc/SpriteData.h
@@ -0,0 +1,13 @@
+#ifndef SQUARE_WAVE_H
+#define SQUARE_WAVE_H
+
+#include <stdint.h>
+
+// Declaration of the square wave array
+extern const uint16_t rainyCloud[];
+extern const uint16_t happySun[];
+extern const uint16_t happyCloud[];
+extern const uint16_t square[];
+
+
+#endif // SQUARE_WAVE_H
